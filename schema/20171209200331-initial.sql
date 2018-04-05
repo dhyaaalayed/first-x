52d21e735a6c613ba0a41dcb3b4b1ebafa96ce61
Dieaa Al Aied <dhyaa.alayed@gmail.com>
2018-04-05 15:09:52 +0200

    add role based access control pattern

--- schema/20171209200331-initial.sql
+++ schema/20171209200331-initial.sql
@@ -12,3 +12,23 @@ CREATE TABLE "user" (
     UNIQUE (id)
 
 );
+
+CREATE TABLE role (
+	id SERIAL PRIMARY KEY NOT NULL,
+	type TEXT NOT NULL UNIQUE,
+	UNIQUE (id)
+);
+
+CREATE TABLE permission (
+	id SERIAL PRIMARY KEY NOT NULL,
+	type TEXT NOT NULL UNIQUE,
+	UNIQUE (id)
+);
+
+CREATE TABLE role_permission (
+	role_id INTEGER NOT NULL REFERENCES role(id),
+	permission_id INTEGER NOT NULL REFERENCES permission(id),
+	PRIMARY KEY (role_id, permission_id),
+	FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
+	FOREIGN KEY (permission_id) REFERENCES permission (id) ON DELETE CASCADE
+);
